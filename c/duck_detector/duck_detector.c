c997f8236d2306bb639db4d42892985bcba91026
myharu01 <63537824+myharu01@users.noreply.github.com>
2022-10-27 12:13:22 -0400

    init update

--- c/duck_detector/duck_detector.c
+++ c/duck_detector/duck_detector.c
@@ -6,6 +6,49 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(){
-    
+void usage()
+{
+    printf("syntax: duck_detector <interface>\n");
+    printf("sample: duck_detector wlan0\n");
+}
+
+typedef struct
+{
+    char *dev_;
+} Param;
+
+Param param = {
+    .dev_ = NULL};
+
+typedef struct attackList
+{
+    u_char mac[6];
+    u_char *attackType;
+    uint8_t pwr;
+};
+
+bool parse(Param *param, int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        usage();
+        return false;
+    }
+    param->dev_ = argv[1];
+    return true;
+}
+
+void showList();
+void showInit(){
+    system("clear");
+    printf("Mac                Atk    PWR\n\n");
+}
+
+int main(int argc, char *argv[])
+{
+    if (!parse(&param, argc, argv))
+        return -1;
+    showInit();
+    while(1)
+        sleep(1);
 }
