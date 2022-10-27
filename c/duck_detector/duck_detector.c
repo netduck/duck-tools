904b5d11bed4126e78b34a9c55a9488116f2b661
myharu01 <63537824+myharu01@users.noreply.github.com>
2022-10-27 15:59:29 -0400

    make show function

--- c/duck_detector/duck_detector.c
+++ c/duck_detector/duck_detector.c
@@ -22,11 +22,26 @@ Param param = {
 
 typedef struct attackList
 {
-    u_char mac[6];
+    u_char *mac;
     u_char *attackType;
     uint8_t pwr;
-};
-
+}attackList;
+void Mac_(const char *arr, u_char mac_addr[6])
+{
+    int a;
+    if (strlen(arr) != 17)
+    {
+        printf("Maclen error!!\n");
+    }
+    char cpyarr[18];
+    memcpy(cpyarr, arr, 17);
+    for (int i = 0; i < 6; i++) //입력Mac값의 콜론 제거
+    {
+        cpyarr[i * 3 + 2] = '\0';
+        sscanf((const char *)&cpyarr[3 * i], "%x", &a);
+        mac_addr[i] = (u_char)a;
+    }
+}
 bool parse(Param *param, int argc, char *argv[])
 {
     if (argc != 2)
@@ -38,17 +53,29 @@ bool parse(Param *param, int argc, char *argv[])
     return true;
 }
 
-void showList();
 void showInit(){
     system("clear");
-    printf("Mac                Atk    PWR\n\n");
+    printf("              Mac    Atk  PWR\n\n");
+}
+
+void showList(int*listCnt,struct attackList*list){
+    showInit();
+    for(int i=0;i<(*listCnt);i++){
+        printf("%s  %5s  -%d\n",(list+i)->mac,(list+i)->attackType,(list+i)->pwr);
+    }
 }
 
 int main(int argc, char *argv[])
 {
     if (!parse(&param, argc, argv))
         return -1;
-    showInit();
-    while(1)
-        sleep(1);
+    struct attackList atkList[5];
+    int cnt = 2;
+    atkList[0].mac = "aa:bb:cc:dd:ee:ff";
+    atkList[0].attackType = "CSA";
+    atkList[0].pwr = 10;
+    atkList[1].mac = "11:22:33:44:55:66";
+    atkList[1].attackType = "DAuth";
+    atkList[1].pwr = 20;
+    showList(&cnt,atkList);
 }
